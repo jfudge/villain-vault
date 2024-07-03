76e63e4761672a8847c9f6277ba16f28aa7cb708
jfudge <email@email.com>
2024-07-03 06:03:29 -0400

    undo

--- users/views.py
+++
@@ -1,8 +0,0 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from .forms import CustomUserCreationForm
-
-class SignUp(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = 'signup.html.django'
-    success_url = reverse_lazy('login')
