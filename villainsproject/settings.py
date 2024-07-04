37d1fd187b8df325122d3ba7b8a94dd25c49d6ab
jfudge <email@email.com>
2024-07-04 04:56:06 -0400

    update render settings

--- villainsproject/settings.py
+++ villainsproject/settings.py
@@ -103,7 +103,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
